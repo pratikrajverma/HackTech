667c8abf8a1a19cc15ff26ccd7c90f460d5be40b
pratik <erpratikrajverma@gmail.com>
2024-06-28 20:18:37 +0530

    allow render from anyware

--- server/index.js
+++ server/index.js
@@ -31,8 +31,8 @@ cloudinaryConnect()
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-            origin: 'http://localhost:3000',
-            // origin: '*',
+            // origin: 'http://localhost:3000',
+            origin: '*',
             credentials:true,
         }));
 app.use(fileupload({
